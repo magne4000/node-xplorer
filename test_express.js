4df7c5bcbda83c1f4b558f29415f3af59231dafa
root <root@mail.getonmyhor.se>
2011-11-02 17:24:23 +0100

    destroying response stream owned by father process

--- test_express.js
+++ test_express.js
@@ -2,6 +2,7 @@ var express = require('express'),
     unixlib = require('unixlib'),
     fs = require('fs'),
     passwd = require('passwd'),
+    nix = require('nix'),
     app = express.createServer();
 
 app.configure(function(){
@@ -16,21 +17,29 @@ app.configure(function(){
 
 app.set('view engine', 'jade'); // Set jade as default render engine
 app.get('/', function(req, res){
+    console.log('Subprocess owned by '+process.getuid()+':'+process.getgid());
     res.render('index.jade', {title: "Login"});
 });
 
 app.post('/login/', function(req, res){
+    console.log('Subprocess owned by '+process.getuid()+':'+process.getgid());
     unixlib.pamauth('system-auth', req.body.user.name, req.body.user.password, function(result) {
         if (result) {
             console.log('User %s logged !', req.body.user.name);
-            passwd.get(req.body.user.name, function(user){
-                process.setgid(parseInt(user.groupId, 10));
-                process.setuid(parseInt(user.userId, 10));
-                console.log('Subprocess owned by '+process.getuid()+':'+process.getgid());
-                fs.readdir(user.homedir, function(err, files){
-                    res.render('login.jade', {title: "Logged", text: ":)", files: files});
+            var pid = nix.fork();
+            if (pid == 0) {
+                // this is the child process, pid = 0
+                passwd.get(req.body.user.name, function(user){
+                    process.setgid(parseInt(user.groupId, 10));
+                    process.setuid(parseInt(user.userId, 10));
+                    console.log('Subprocess owned by '+process.getuid()+':'+process.getgid());
+                    fs.readdir(user.homedir, function(err, files){
+                        res.render('login.jade', {title: "Logged", text: ":)", files: files});
+                    });
                 });
-            });
+            } else {
+                res.destroy();
+            }
         }else{
             res.render('login.jade', {title: "Not logged", text: ":("});
         }
